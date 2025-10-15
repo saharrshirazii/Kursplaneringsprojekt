b94bcb8804895b723b9175a1cf436e7005040d0c
saharrshirazii <saharr.shirazii@gmail.com>
2025-10-15 12:26:32 +0200

    I add a image in footer and update my javascript file.

---
+++ sahar.js
@@ -0,0 +1,7 @@
+//getElementByClassName("sahar-class")
+const saharImage = document.getElementsByClassName("sahar-class")[0]; // Select the first element with the class name "sahar-class"
+
+// Add an event listener for double-click events;
+saharImage.addEventListener("dblclick", function() {
+    alert("Hi Everyone! It is my github link: https://github.com/saharrshirazii");
+}); 
